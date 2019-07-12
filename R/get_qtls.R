9f2a3238e674679378745a61941fad7afe702c8a
JuliaR1 <julia.remes@student.uni-luebeck.de>
2019-07-12 13:17:26 +0200

    add parameter max_term, only max_term queries are made at a time

--- R/get_qtls.R
+++ R/get_qtls.R
@@ -10,37 +10,28 @@
 #'get_qtls("rs4284742", 0.6)
 #'@export
 get_qtls <- function(query, r2 = 0.8){
-    #Note: in the current version of Qtlizer, queries including 
-    #gene identifier and chromosomal positions
-    #of variants taker longer than if using rsids only
-    len = length(query)
-    if(len == 1){ #single string
-        q <- query
-    } else{
-        q <- paste(query, collapse = ' ') #make a single query
-    }
-    
-    #corr <- 0.8 # optional zw 0 und 1 r2 nennen
+  
+  #inside function that actually makes the queries
+  mkQuery <- function(q, r2 = 0.8){
     ld_method <- "r2" # optional default
-    print("connecting to Qtlizer...  ")
+    print(paste("connecting to Qtlizer with query ", q, " ......"))
     url <- paste('http://genehopper.de/rest/qtlizer?q=', gsub("\\s+", ",", q), 
-        "&corr=", r2, "&ld_method=", ld_method, sep="")
+                 "&corr=", r2, "&ld_method=", ld_method, sep="")
+    print(paste("URL--------------------", url))
     response <- httr::POST(url)
     result <- httr::content(response)
     
-    print("processing results...")
     a <- unlist(strsplit(result , "\n"))
     meta <- grep("^#", a, value = TRUE)
     data <- grep("^[^#]", a, value = TRUE)
     header <- unlist(strsplit(data[1], "\t"))
     ncols <- length(header)
-
+    
     if(is.null(data[-1])){
-        data <- NA
+      data <- NA
     } else {
-        data <- unlist(strsplit(data[-1], "\t"))
+      data <- unlist(strsplit(data[-1], "\t"))
     }
-
     m <- matrix(data, ncol=ncols, byrow=TRUE)
     d <- as.data.frame(m, stringsAsFactors=FALSE)
     d[d=="-"] <- NA
@@ -48,5 +39,28 @@ get_qtls <- function(query, r2 = 0.8){
     comment(d) = meta
     print("finished")
     return(d)
+  } 
+    #Note: in the current version of Qtlizer, queries including 
+    #gene identifier and chromosomal positions
+    #of variants taker longer than if using rsids only
+  
+    #convert convert into the desired shape
+    len = length(query)
+    if(len == 1){ #single string
+        q <- query
+    } else{
+        q <- paste(query, collapse = ' ') #make a single query
+    }
+    
+    max_terms = 2 #maximum number of queries made at a time
+    spltq <- unlist(strsplit(q, " "))
+    y <- matrix(spltq,  ncol=max_terms)
+    s <- apply(y,1,paste,collapse=" ")
+    #q <- lapply(s, mkQuery)
+    
+    return(lapply(s, mkQuery, r2 = r2))
+    
+
+    
 
 }
