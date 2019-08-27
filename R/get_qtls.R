c171be3b8767e36b0a1f384cd152b659326fa90f
Matthias Munz <matthias.munz@gmx.de>
2019-08-27 12:31:17 +0200

    Additional error handling

--- R/get_qtls.R
+++ R/get_qtls.R
@@ -2,21 +2,19 @@
 #'
 #'@description Query Qtlizer database for expression quantitative trait loci (eQTLs) in human.
 #'
-#'@param query The query. Can either be a single string or a vector.
+#'@param query The query consists of search terms and can be a single string or a vector.
 #' Qtlizer allows to query both variants (Rsid, ref_version:chr:pos) and
-#'  genes (Symbol consisting of letters and numbers according to the HGNC guidelines).
-#'@param corr Linkage disequilibrium based on 1000Genomes Phase 3 European.
-#' Optional value between 0.1 and 1. Default value is NA. 
-#'@param max_terms Number of queries made at a time. The default value is 5. 
+#'  genes (Symbol consisting of letters and numbers according to the HGNC guidelines). Minimum allowed term length is 2.
+#'@param corr Linkage disequilibrium based on 1000 Genomes Phase 3 European. If this optional value between 0 and 1 is set, 
+#'the input variants are enriched for proxy variants passing the threshold. Default value is NA. 
+#'@param max_terms Number of terms in a single HTTP request. Default value is 5. 
 #'A large value can lead to a very large result set and a error by the database.
-#'@param ld_method There are two methods. Default method is "r2". 
-#'The other opportunity is to use "dprime".
-#'@param ref_version Two possible versions to use hg19 or hg38. Default value
-#'is hg19.
-#'@param return_obj The user can choose to get the output as a dataframe or 
-#'as a GRange object. The default value is the dataframe. If a GRange object is 
-#'wanted use return_obj = "grange". 
-#'@return Data frame with response.
+#'@param ld_method There are two methods available: "r2" (default) and "dprime".
+#'@param ref_version Two possible versions are supported: hg19 (GRCh37) or hg38 (GRCh38). Default value
+#'is "hg19". This argument is only considered if a GRange is returned.
+#'@param return_obj The user can choose to get the QTL data to be returned as data frame or 
+#'as a GRange object. The default value is "dataframe".
+#'@return Data frame or GRange object with QTL data.
 #'@examples get_qtls("rs4284742")
 #'get_qtls(c("rs4284742", "DEFA1"))
 #'get_qtls(c("rs4284742,DEFA1"))
@@ -25,24 +23,24 @@
 #'get_qtls("rs4284742", corr=0.2, ld_method = "dprime")
 #'@export
 get_qtls = function(query, corr = NA, max_terms = 5, ld_method = "r2", 
-                     ref_version = "hg19", return_obj = "dataframe"){
+                    ref_version = "hg19", return_obj = "dataframe"){
   
   # Check if there is an internet connection
   if (!curl::has_internet())
     stop("no internet connection detected...")
-    
-   
+  
+  
   # Split terms
   query = paste(query, sep = ' ', collapse = ' ')
   query = unique(unlist(strsplit(query, "[\\n\\s,\\t.;]+", perl=TRUE)))
+  query = query[stringi::stri_length(query)>1]
   message(length(query)," unique query term(s) found")
   
   
-  # Binning
-  print(query)
+  # Binningtolower
   bins = vector_split(query, ceiling(length(query)/max_terms))
   bins = stringi::stri_join_list(bins, sep = ",")
-
+  
   
   # Communicate with database
   message("Retrieving QTL information from Qtlizer...")
@@ -50,7 +48,7 @@ get_qtls = function(query, corr = NA, max_terms = 5, ld_method = "r2",
   res = do.call(rbind, res)
   
   if(!is.null(res)){
-    message(nrow(res), " data points received")
+    message(nrow(res), " data point(s) received")
   } else {
     return(res)
   }
@@ -69,14 +67,14 @@ get_qtls = function(query, corr = NA, max_terms = 5, ld_method = "r2",
   
   
   # Create GRange container
-  if(return_obj == "grange"){
+  if(tolower(return_obj) == "grange"){
     
-    if(ref_version == "hg19"){
+    if(tolower(ref_version) == "hg19" || tolower(ref_version) == "grch37"){
       
       # Check for missing hg19 positions
       res_with_hg19 = res[which(!is.na(res$var_pos_hg19)),] 
       if(nrow(res_with_hg19) < nrow(res)){
-          message("Not all results in GRange object included due to missing hg19 positions. Please use set ref_version to 'hg38' andd/or set return_obj to 'dataframe' to obtain all results.")
+        message("Not all results in GRange object included due to missing hg19 (GRCh37) positions. Please use set ref_version to hg38 (GRCh38) andd/or set return_obj to 'dataframe' to obtain all results.")
       }
       
       gres = GenomicRanges::makeGRangesFromDataFrame(res_with_hg19, start.field = "var_pos_hg19", end.field = "var_pos_hg19", 
@@ -112,16 +110,26 @@ mkQuery = function(q, corr, ld_method){
   url = paste0('http://genehopper.de/rest/qtlizer?q=', q)
   
   if(is.numeric(corr) && corr>=0 && corr<=1){
-    url = paste0(url, "&corr=", corr)
+    url = paste0(url, "&corr=", round(corr, digits=2))
+    
+    if(is.character(ld_method) && (ld_method == "r2" || corr == "dprime")){
+      url = paste0(url, "&ld_method=", ld_method)
+    }
   }
-  if(is.character(ld_method) && (ld_method == "r2" || corr == "dprime")){
-    url = paste0(url, "&ld_method=", ld_method)
-  }
-
+  
   
   # Send post request and retrieve response
   response = httr::POST(url)
   result = httr::content(response)
+  if(!is.character(result)){
+    
+    title = rvest::html_text(rvest::html_nodes(result, "title"))
+    h1 = rvest::html_text(rvest::html_nodes(result, "h1"))
+    mess = rvest::html_text(rvest::html_nodes(result, "p"))
+    
+    warning(paste(title, h1, mess, sep="\n"))
+    return(NULL)
+  }
   result = unlist(strsplit(result , "\n"))
   
   
@@ -148,7 +156,7 @@ mkQuery = function(q, corr, ld_method){
   header = unlist(strsplit(data[1], "\t"))
   ncols = length(header)
   data = unlist(strsplit(data[-1], "\t"))
-    
+  
   m = matrix(data, ncol=ncols, byrow=TRUE)
   d = as.data.frame(m, stringsAsFactors=FALSE)
   colnames(d) = header
