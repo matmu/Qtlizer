185aa88eefe09e285a4e5c8b745f762deaee4c47
Matthias Munz <matthias.munz@gmx.de>
2026-03-22 17:25:10 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,7 @@
   Munz M et al. (2020) Qtlizer: comprehensive QTL annotation of GWAS results. Scientific Reports. doi:10.1038/s41598-020-75770-7
   Munz M. et al. (2015) Multidimensional gene search with Genehopper. Nucleic Acids Res. doi:10.1093/nar/gkv511
 
-  Support me: https://matthiasmunz.de/support_me/
+  Support me: https://matthiasmunz.de/en/thanks/
 
   ---------", domain = NULL, appendLF = TRUE)
 }
